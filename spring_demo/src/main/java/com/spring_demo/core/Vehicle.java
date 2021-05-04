cb87831ea167e66952205ef867928bf0e77a0830
mauryaajay36024 <mauryaajay36024@gmail.com>
2021-05-05 00:54:15 +0530

    .gitignore updated

---
+++ spring_demo/src/main/java/com/spring_demo/core/Vehicle.java
@@ -0,0 +1,2 @@
+package com.spring_demo.Dao;public class Vehicle {
+}
---
+++ spring_demo/src/main/java/com/spring_demo/model/MysqlClient.java
@@ -0,0 +1,12 @@
+package com.spring_demo.Service;
+
+import com.spring_demo.Core.Vehicle;
+
+public class MysqlClient {
+  Vehicle vehicle=new Vehicle();
+  public void registerVehicle(){
+
+
+  }
+
+}
