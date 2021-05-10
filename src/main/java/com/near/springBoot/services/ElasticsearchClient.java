a1d9fe71156a96704b1ccc92eee707198f5b99de
mauryaajay36024 <mauryaajay36024@gmail.com>
2021-05-10 17:48:24 +0530

    API's for Elasticsearch client

--- src/main/java/com/near/springBoot/services/ElasticsearchClient.java
+++ src/main/java/com/near/springBoot/services/ElasticsearchClient.java
@@ -1,6 +1,6 @@
 package com.near.springBoot.services;
 import com.near.springBoot.entity.Vehicle;
-import com.near.springBoot.repository.RedisRepository;
+import com.near.springBoot.repository.VehicleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.HashMap;
@@ -9,41 +9,41 @@ import java.util.Optional;
 
 
 @Service
-public class RedisClient implements DbService {
+public class ElasticsearchClient implements DbService {
 
   @Autowired
-  private RedisRepository redisRepository;
+  private VehicleRepository vehicleRepository;
 
   @Override
   public Iterable<Vehicle> printInfo() {
 
-    return redisRepository.findAll();
+    return vehicleRepository.findAll();
   }
 
   @Override
   public Vehicle addInfo(Vehicle vehicle) {
-    redisRepository.save(vehicle);
+    vehicleRepository.save(vehicle);
     return vehicle;
   }
 
   @Override
-  public Optional<Vehicle> updateInfo(Long  id,Vehicle vehicleDetails){
-     Optional<Vehicle> veh = redisRepository.findById(id);
+  public Optional<Vehicle> updateInfo(String  id,Vehicle vehicleDetails){
+     Optional<Vehicle> veh = vehicleRepository.findById(id);
      if(veh.isPresent()){
        Vehicle vehicle=veh.get();
        vehicle.setRegNo(vehicleDetails.getRegNo());
        vehicle.setColour(vehicleDetails.getColour());
-       redisRepository.save(vehicle);
+       vehicleRepository.save(vehicle);
      }
      return veh;
 
   }
 
   @Override
-  public Map<String, Boolean> deleteInfo(Long id) {
-    Optional<Vehicle> veh=redisRepository.findById(id);
+  public Map<String, Boolean> deleteInfo(String id) {
+    Optional<Vehicle> veh=vehicleRepository.findById(id);
     Vehicle vehicle=veh.get();
-    redisRepository.delete(vehicle);
+    vehicleRepository.delete(vehicle);
     Map<String,Boolean> response=new HashMap<>();
     response.put("Data Deleted",Boolean.TRUE);
     return response;
