f5676e84c2cf718dac1b36e3dfee288b8cec2902
mauryaajay36024 <mauryaajay36024@gmail.com>
2021-05-17 08:25:02 +0530

    entity for mongodb created

--- src/main/java/com/near/parkingSystem/mongodbDatabase/entity/Vehicle.java
+++ src/main/java/com/near/parkingSystem/mongodbDatabase/entity/Vehicle.java
@@ -1,8 +1,8 @@
 package com.near.parkingSystem.mongodbDatabase.entity;
 
-
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import javax.persistence.Id;
+
 
 @Document(collection= "parking_system")
 public class Vehicle {
