db0a8ba616db3988443ba7f522d7178ce674c0db
mauryaajay36024 <mauryaajay36024@gmail.com>
2021-05-17 08:28:02 +0530

    mysql config class added

---
+++ src/main/java/com/near/parkingSystem/config/MysqlConfig.java
@@ -0,0 +1,64 @@
+package com.near.parkingSystem.config;
+
+import com.near.parkingSystem.mysqlDatabase.entity.Vehicle;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = "com.near.parkingSystem.mysqlDatabase.repository",
+    entityManagerFactoryRef = "entityManagerFactory",
+    transactionManagerRef = "transactionManager"
+)
+public class MysqlConfig {
+
+  @Bean
+  @Primary
+  @ConfigurationProperties("spring.datasource.mysql")
+  public DataSourceProperties memberDataSourceProperties() {
+    return new DataSourceProperties();
+  }
+
+  @Primary
+  @Bean(name="datasource")
+  @ConfigurationProperties(prefix = "spring.datasource.mysql.configuration")
+  //Datasource
+  public DataSource dataSource(){
+    return memberDataSourceProperties().initializeDataSourceBuilder()
+        .type(HikariDataSource.class).build();
+  }
+
+  @Primary
+  @Bean(name="entityManagerFactory")
+  //EMF
+  public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean(EntityManagerFactoryBuilder builder,
+                                                                         @Qualifier("datasource") DataSource dataSource)
+  {
+    return builder
+        .dataSource(dataSource)
+        .packages(Vehicle.class)
+        .build();
+  }
+
+  @Primary
+  @Bean(name="transactionManager")
+  public PlatformTransactionManager transactionManager(
+      @Qualifier("entityManagerFactory") EntityManagerFactory entityManagerFactory
+  )
+  {
+    return new JpaTransactionManager(entityManagerFactory);
+  }
+}
